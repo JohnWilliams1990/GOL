17ed9d2ebea46c12058873bff776628e16ea4d23
John Williams <Johnwilliams199032@gmail.com>
2018-08-12 00:52:31 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Python Game of life for python 2.7
---
+++ game_of_life.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python2.7
+#  John Williams
+#  105201054
+#  Date: Sat. Aug. 11, 2018
+#  Program: game_of_life.py
+
+
+board= [["-"]*30]*30
+
+
+#for i in board:
+#	i= "-"*30
+#
+
+for i in board:
+	for j in i:
+		j = "-"
+
+for in board:
+	for j in i:
+		print j, end=" " 
+#	print ("")
+
+
